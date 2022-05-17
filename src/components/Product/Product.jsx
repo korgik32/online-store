bd5d9d9ad92d0bdc9db77f513e154256f5435692
korgik32 <mishahero@mail.ru>
2022-05-17 21:56:30 +0300

    changed like

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -9,9 +9,10 @@ const Product = () => {
             <img alt='(((' src="https://placepic.ru/wp-content/uploads/2021/01/FF3mH8WjNnC2WlfFnSX34PFQycxOoEx0ViBmDS867A1Qs4DKSXt3TF2MYn0vLX7GiqgYZDBv9IU7JAx6FA3wqAAb7QWJ9yUh8QwVhux99aPsSJ7HTqAEaFt0IRIuOqsSG7CN9hQ.jpg" className={s.product__image}>
 
             </img>
-            <button className={s.like}>
+            {/* <button className={s.like}>
                 <img src='/img/likes.svg' alt='((('></img>
-            </button>
+            </button> */}
+            <img className={s.like} src='/img/likes.svg' alt='((('></img>
             <div className={s.profuct__name}>Мужские Кроссовки Nike Blazer Mid Suede</div>
             <div className={s.product__price}>
                 <section>
